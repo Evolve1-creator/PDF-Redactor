b78d575e2f90756059a439e7f7d42a16050c5167
Evolve1-creator <tammyp@evolve-business.com>
2026-02-18 10:00:24 -0500

    Delete backend directory

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# If Tesseract is not on PATH (common on Windows), set this env var:
-#   setx TESSERACT_CMD "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
-TESSERACT_CMD = os.getenv("TESSERACT_CMD", "").strip()
-
-# Maximum upload size can be enforced at reverse proxy; FastAPI itself doesn't hard-stop large uploads by default.

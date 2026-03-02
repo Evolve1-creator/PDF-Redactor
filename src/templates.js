7dd8864e2bbf106cab8baa23334a548525c0155c
Evolve1-creator <tammyp@evolve-business.com>
2026-03-02 15:29:01 -0500

    add page 2+ at 0.63

--- src/templates.js
+++ src/templates.js
@@ -16,7 +16,7 @@ export const TEMPLATES = {
   surgery_notes: {
     name: "Surgery Notes",
     mode: "top_band_inches",
-    topBandInches: { firstPage: 1.75, otherPages: 0.0 }
+    topBandInches: { firstPage: 1.75, otherPages: 0.63 }
   },
 
   asante_notes: {
